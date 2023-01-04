b99659ca0ec7e932de73736355a21b14037ccbeb
Pooryamb <pooryamb@gmail.com>
2023-01-04 14:38:01 -0500

    The model was trained to work with e-value=1, however, it wasn't efficient enough

--- scripts/AddingPredProbAndLabel2DF.py
+++ scripts/AddingPredProbAndLabel2DF.py
@@ -4,13 +4,12 @@ def AddingPredProbAndLabel2DF(X, model):
     PredProb  = model.predict_proba(X[:,:9])[:, 1]
     PredLabel = model.predict(X[:,:9])
     df = pd.DataFrame(data = X, columns = ['fident', 'alnlen', 'MismatchRatio', 'GapOpenRatio', 'tlen', 'bits',
-       'alntmscore','lddt', "FracOfPf","p_evalue", 'query', 'target','qstart',
-       'qend', 'tstart', 'tend', 'qlen','PredPF', 'PF', 'PFstart', 'PFend', 'evalue', 'Status'])
+       'alntmscore', 'lddt', 'FracOfPf', 'p_evalue','query','qstart', 'qend','PredPF', 'PF',"Status"])
     df["PredLabel"] = PredLabel
     df["RF_prob"] = PredProb
     df = df[df["PredLabel"]==1]
     df.sort_values(by=["query", "RF_prob"], ascending=[True, False], ignore_index = True, inplace=True)
-    dfNewOrder = df[['query','PredPF','qstart', 'qend',"RF_prob", "PredLabel",  
-        'fident', 'alnlen', 'MismatchRatio', 'GapOpenRatio', 'qlen', 'bits',
-        'alntmscore', "FracOfPf","p_evalue", 'PF', 'PFstart', 'PFend', 'evalue', 'Status']]
+    df = df[['query','PredPF','qstart', 'qend',"RF_prob", "PredLabel",  
+        'fident', 'alnlen', 'MismatchRatio', 'GapOpenRatio', 'tlen', 'bits',
+        'alntmscore', "FracOfPf","p_evalue", 'PF',"Status"]]
     return df
