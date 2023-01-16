fea082fa2b31f8545c03fc32f82d31e875e0e5a4
Pooryamb <pooryamb@gmail.com>
2023-01-16 10:03:30 -0500

    Removed files that are not needed anymore

--- scripts/OverLapReturner.py
+++
@@ -1,5 +0,0 @@
-def OverLapReturner(Interval1, Interval2):
-    """It takes two intervals as input and returns the overlap between them as output"""
-    MaxStart = max(Interval1[0], Interval2[0])
-    MinEnd  =  min(Interval1[1], Interval2[1])
-    return max(MinEnd-MaxStart+1,0)
